73235bc1ae5a4d12bb6954530c37724c2b9e9afb
Kerem Mimir <mimirkerem@gmail.com>
2024-08-05 18:01:09 +0300

    InternTasks-4A | Add navigation args and pass data

--- app/src/main/java/com/example/interntasks_4a/View/CharacterDetailFragment.kt
+++ app/src/main/java/com/example/interntasks_4a/View/CharacterDetailFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.interntasks_4a.databinding.FragmentCharacterDetailBinding
 
 class CharacterDetailFragment : Fragment() {
@@ -12,12 +14,38 @@ class CharacterDetailFragment : Fragment() {
     private  var _binding: FragmentCharacterDetailBinding? = null
     private val binding get() = _binding!!
 
+    private val args by navArgs<CharacterDetailFragmentArgs>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentCharacterDetailBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding){
+            Glide.with(view.context)
+                .load(args.character.image)
+                .override(500,500)
+                .into(imageDetail)
+
+            fullNameText.text = args.character.fullName
+            nickname.text = args.character.nickname
+            hogwartsHouse.text = args.character.hogwartsHouse
+            interpretedBy.text = args.character.interpretedBy
+
+            val childrenArgs = args.character.children
+            if (childrenArgs != null) {
+                val concatenatedChildren = childrenArgs.joinToString("\n")
+                children.text = concatenatedChildren
+            }
+
+            birthdate.text = args.character.birthdate
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/interntasks_4a/View/CharacterListFragment.kt
+++ app/src/main/java/com/example/interntasks_4a/View/CharacterListFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.example.interntasks_4a.Adapter.CharacterAdapter
 import com.example.interntasks_4a.Model.CharacterModel
 import com.example.interntasks_4a.ViewModel.CharacterViewModel
@@ -18,6 +19,7 @@ class CharacterListFragment : Fragment() {
     private var _binding: FragmentCharacterListBinding? = null
     private val binding get() = _binding!!
     private val viewModel: CharacterViewModel by viewModels()
+    private lateinit var adapter: CharacterAdapter
     val characterList = ArrayList<CharacterModel>()
 
     override fun onCreateView(
@@ -31,16 +33,24 @@ class CharacterListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        observerData()
+        adapter = CharacterAdapter(characterList)
+        binding.recylerView.adapter = adapter
+
         viewModel.getData()
+        observerData()
     }
 
     fun observerData(){
         viewModel.characters.observe(viewLifecycleOwner, Observer { character ->
             character?.let {
-                characterList.addAll(character)
-                val adapter = CharacterAdapter(characterList)
-                binding.recylerView.adapter = adapter
+                characterList.clear()
+                characterList.addAll(it)
+                adapter.notifyDataSetChanged()
+
+                adapter.onClick = { characterModel ->
+                    val action = CharacterListFragmentDirections.actionCharacterListFragmentToCharacterDetailFragment(character = characterModel)
+                    findNavController().navigate(action)
+                }
             }
         })
         viewModel.error.observe(viewLifecycleOwner, Observer { errorMessage ->
